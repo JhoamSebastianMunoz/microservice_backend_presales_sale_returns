32ff300f9f3a8293f3843b41d631353df4808ca8
JhoamSebastianMunoz <jhoamsebastian68@gmail.com>
2025-02-19 10:48:04 -0500

    modified

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,13 +3,20 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
+const sslConfig = process.env.DB_SSL === 'true'
+  ? { rejectUnauthorized: false } // o true si cuentas con el certificado adecuado
+  : undefined;
+
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    connectionLimit: 10,
-    queueLimit: 0
-  });
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: Number(process.env.DB_PORT) || 3306,
+  connectionLimit: 10,
+  queueLimit: 0,
+  ssl: sslConfig
+});
+
   
 export default db.promise()
